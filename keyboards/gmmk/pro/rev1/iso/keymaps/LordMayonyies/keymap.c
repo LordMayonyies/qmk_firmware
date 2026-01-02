e76367ccf4a6d92b53371c5b97e9270b4b5ac622
LordMayonyies <supermayo17@gmail.com>
2026-01-02 13:24:03 +0100

    Add gaming layer to remove mod-tap

--- keyboards/gmmk/pro/rev1/iso/keymaps/LordMayonyies/keymap.c
+++ keyboards/gmmk/pro/rev1/iso/keymaps/LordMayonyies/keymap.c
@@ -18,7 +18,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 enum layer_names {
     _Main = 0,
-    _Functions = 1
+    _Gaming = 1,
+    _Functions = 2
 };
 
 // clang-format off
@@ -55,21 +56,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     // Main layer
     [_Main] = LAYOUT(
-        KC_ESC,  KC_F1,           KC_F2,           KC_F3,           KC_F4,           KC_F5,         KC_F6,            KC_F7,           KC_F8,           KC_F9,    KC_F10,         KC_F11,  KC_F12,  KC_DEL,   KC_MUTE,
-        KC_GRV,  KC_1,            KC_2,            KC_3,            KC_4,            KC_5,           KC_6,            KC_7,            KC_8,            KC_9,     KC_0,           KC_MINS, KC_EQL,  KC_BSPC,  KC_PGUP,
-        KC_TAB,  KC_Q,            KC_W,            KC_E,            KC_R,            KC_T,           KC_Y,            KC_U,            KC_I,            KC_O,     KC_P,           KC_LBRC, KC_RBRC,           KC_PGDN,
-        KC_CAPS, KC_A,     LSFT_T(KC_S),    LALT_T(KC_D),    LGUI_T(KC_F),    LCTL_T(KC_G),   RCTL_T(KC_H),    RGUI_T(KC_J),    RALT_T(KC_K),    RSFT_T(KC_L),    KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT,          KC_HOME,
-        KC_LSFT, KC_NUBS,         KC_Z,            KC_X,            KC_C,            KC_V,           KC_B,            KC_N,            KC_M,            KC_COMM,  KC_DOT,    KC_SLSH, KC_RSFT, KC_UP,         KC_END,
-        KC_LCTL, KC_LALT,         KC_LGUI,                                                           KC_SPC,                                            KC_RGUI, MO(1),   KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
+        KC_ESC,  KC_F1,           KC_F2,           KC_F3,           KC_F4,           KC_F5,         KC_F6,            KC_F7,           KC_F8,           KC_F9,    KC_F10,           KC_F11,  KC_F12,   KC_DEL,   KC_MUTE,
+        KC_GRV,  KC_1,            KC_2,            KC_3,            KC_4,            KC_5,           KC_6,            KC_7,            KC_8,            KC_9,     KC_0,             KC_MINS, KC_EQL,  KC_BSPC,   KC_PGUP,
+        KC_TAB,  KC_Q,            KC_W,            KC_E,            KC_R,            KC_T,           KC_Y,            KC_U,            KC_I,            KC_O,     KC_P,             KC_LBRC, KC_RBRC,            KC_PGDN,
+        KC_CAPS, KC_A,     LSFT_T(KC_S),    LALT_T(KC_D),    LGUI_T(KC_F),    LCTL_T(KC_G),   RCTL_T(KC_H),    RGUI_T(KC_J),    RALT_T(KC_K),    RSFT_T(KC_L),    KC_SCLN,          KC_QUOT, KC_NUHS,  KC_ENT,   KC_HOME,
+        KC_LSFT, KC_NUBS,         KC_Z,            KC_X,            KC_C,            KC_V,           KC_B,            KC_N,            KC_M,            KC_COMM,  KC_DOT,           KC_SLSH, KC_RSFT,   KC_UP,   KC_END,
+        KC_LCTL, KC_LALT,         KC_LGUI,                                                           KC_SPC,                                            KC_RGUI,  MO(_Functions),   KC_RCTL,   KC_LEFT, KC_DOWN, KC_RGHT
+    ),
+    [_Gaming] = LAYOUT(
+        KC_ESC,  KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5,  KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,         KC_F11,  KC_F12,  KC_DEL,   KC_MUTE,
+        KC_GRV,  KC_1,     KC_2,    KC_3,    KC_4,    KC_5,   KC_6,    KC_7,    KC_8,    KC_9,     KC_0,           KC_MINS, KC_EQL,  KC_BSPC,  KC_PGUP,
+        KC_TAB,  KC_Q,     KC_W,    KC_E,    KC_R,    KC_T,   KC_Y,    KC_U,    KC_I,    KC_O,     KC_P,           KC_LBRC, KC_RBRC,           KC_PGDN,
+        KC_CAPS, KC_A,     KC_S,    KC_D,    KC_F,    KC_G,   KC_H,    KC_J,    KC_K,    KC_L,     KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT,          KC_HOME,
+        KC_LSFT, KC_NUBS,  KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,    KC_N,    KC_M,    KC_COMM,  KC_DOT,    KC_SLSH, KC_RSFT, KC_UP,         KC_END,
+        KC_LCTL, KC_LGUI,  KC_LALT,                           KC_SPC,                    KC_RALT,  MO(2),   KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
     ),
     // FN layer
     [_Functions] = LAYOUT(
-        QK_BOOT, KC_BRID, KC_BRIU, _______, _______, RM_VALD, RM_VALU, KC_MPRV, KC_MPLY, KC_MNXT, _______, _______, _______, _______,          _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, AG_TOGG, _______,          _______,
-        _______, _______, _______, _______, EE_CLR, _______, _______, _______, _______, _______, _______, _______, _______,                   _______,
-        _______, _______, _______, _______, _______, _______, RM_TOGG, RM_SATD, RM_SATU, RM_HUEU, _______, _______, _______, _______,          _______,
-        _______, _______, _______, _______, _______, _______, _______, NK_TOGG, _______, _______, _______, _______,          _______, RM_NEXT, _______,
-        _______, _______, _______,                               _______,                         _______, _______, _______, RM_SPDD, RM_PREV, RM_SPDU
+        QK_BOOT, KC_BRID,    KC_BRIU,      _______, _______, RM_VALD, RM_VALU, KC_MPRV, KC_MPLY, KC_MNXT, _______, _______, _______, _______,          _______,
+        _______, DF(_Main),  DF(_Gaming),  _______, _______, _______, _______, _______, _______, _______, _______, _______, AG_TOGG, _______,          _______,
+        _______, _______,    _______,      _______, EE_CLR,  _______, _______, _______, _______, _______, _______, _______, _______,                   _______,
+        _______, _______,    _______,      _______, _______, _______, RM_TOGG, RM_SATD, RM_SATU, RM_HUEU, _______, _______, _______, _______,          _______,
+        _______, _______,    _______,      _______, _______, _______, _______, NK_TOGG, _______, _______, _______, _______,          _______, RM_NEXT, _______,
+        _______, _______,    _______,                                    _______,                         _______, _______, _______, RM_SPDD, RM_PREV, RM_SPDU
     )
 };
 // clang-format on
@@ -167,11 +176,11 @@ layer_state_t layer_state_set_user(layer_state_t state) {
     uint8_t layer = get_highest_layer(state);
 
     switch (layer) {
-    case _Main:
-        // rgb_matrix_mode(RGB_MATRIX_CUSTOM_mac_mode_effect);
-        break;
-    default: //  for any other layers, or the default layer
-        break;
+        case _Main:
+            // rgb_matrix_mode(RGB_MATRIX_CUSTOM_mac_mode_effect);
+            break;
+        default: //  for any other layers, or the default layer
+            break;
     }
 
   return state;
